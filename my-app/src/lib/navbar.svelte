a73d83101bf0594717590e61474c4bdf43eefa2b
RokasPazera <156358230+RokasPazera@users.noreply.github.com>
2025-01-31 21:20:25 +0100

    Delete my-app directory

--- my-app/src/lib/navbar.svelte
+++
@@ -1,6 +0,0 @@
-<nav class="bg-primaryNavigation border-gray-200">
-    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <a href="/about">About</a>
-    <a href="/login">Log in</a>
-    </div>
-</nav>
